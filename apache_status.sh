72cd1590e907abe45798c32222cba1b63874f33f
Philip Gates-Crandall <philip.gates-crandal@rackspace.com>
2014-04-22 10:48:59 -0500

    initial apache server status based on standard install

---
+++ apache_status.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## Rackspace Cloud Monitoring Plug-In
+## MySQL Replication State Validation
+#
+# (C)2013 Philip Gates-Crandall <philip.gates-crandal@rackspace.com>
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Usage:
+# Place plug-in in /usr/lib/rackspace-monitoring-agent/plugins
+#
+
+STATUS=$(curl -s http://localhost/server-status?auto)
+IFS=$'\n'
+
+echo 'status OK'
+for item in $STATUS
+do
+  if [[ $item == *Scoreboard* ]]
+    then
+      echo $item | awk -F':' '{gsub(" ", "", $1);print "metric "$1" string "$2}'
+  else
+      echo $item | awk -F':' '{gsub(" ", "", $1);print "metric "$1" int "$2}'
+  fi
+done
+
