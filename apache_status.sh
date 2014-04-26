bb2bfbf278c0279cca23bfaf95bd9c63307b1b6c
Philip Gates-Crandall <philip.gates-crandal@rackspace.com>
2014-04-26 16:18:30 -0500

    add option flags and basic error handling

--- apache_status.sh
+++ apache_status.sh
@@ -22,25 +22,70 @@
 # Place plug-in in /usr/lib/rackspace-monitoring-agent/plugins
 #
 
-USER=$1
-PASSWORD=$2
+show_help() {
+cat << EOF
+USAGE: $0 [-h] [-u username:password] [-l location] [-p port] [-m timeout]
 
-if ! [[ -z $USER && -z $PASSWORD ]]
-then
-  STATUS=$(curl -u ${1}:${2} -s http://localhost/server-status?auto)
-else
-  STATUS=$(curl -s http://localhost/server-status?auto)
-fi
-IFS=$'\n'
+  -h print this help summary
+  -u username:password - HTTP Authentication for curl (default none)
+  -l location - the location of the Apache server-status module (default "/server-status?auto")
+  -p port - the port that Apache is listening on (default 80)
+  -m timeout - the length of time to try the request (default 30 seconds)
+EOF
+}
 
-echo 'status OK'
-for item in $STATUS
-do
-  if [[ $item == *Scoreboard* ]]
-    then
-      echo $item | awk -F':' '{gsub(" ", "", $1);print "metric "$1" string "$2}'
-  else
-      echo $item | awk -F':' '{gsub(" ", "", $1);print "metric "$1" int "$2}'
-  fi
+location="/server-status?auto"
+port="80"
+host="localhost"
+timeout="30 seconds"
+
+while getopts "u:l:p:m:h?" o; do
+    case "${o}" in
+        h)  show_help
+            exit 0
+            ;;
+        u) user=${OPTARG}
+           CURL_OPTS=${CURL_OPTS}" -u ${user}"
+            ;;
+        l) location=${OPTARG}
+            ;;
+        p) port=${OPTARG}
+           CURL_HOST="${hostname}:${port}${location}"
+            ;;
+        m) timeout=${OPTARG}
+           CURL_OPTS=${CURL_OPTS}" -m ${timeout}"
+            ;;
+        '?') show_help
+             exit 0
+            ;;
+    esac
 done
+
+CURL_TARGET="http://${host}:${port}${location}"
+
+STATUS=$(curl -s${CURL_OPTS} ${CURL_TARGET})
+
+if [[ -z $STATUS ]]
+then
+  echo "status ERROR"
+  echo "metric STATUS string received no response from ${CURL_TARGET}"
+  exit 1
+elif [[ $STATUS != *Uptime* ]]
+then
+  echo "status ERROR"
+  echo "metric STATUS string no receiving expected output from ${CURL_TARGET}"
+  exit 1
+else
+  IFS=$'\n'
+  echo 'status OK'
+  for item in $STATUS
+  do
+    if [[ $item == *Scoreboard* ]]
+      then
+        echo $item | awk -F':' '{gsub(" ", "", $1);print "metric "$1" string "$2}'
+    else
+        echo $item | awk -F':' '{gsub(" ", "", $1);print "metric "$1" int "$2}'
+    fi
+  done
+fi
 
